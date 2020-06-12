ee6206f6fa5e58a05428cb8d1f3df2a1f2f53b59
Victor San Kho Lin <victor@sankholin.com>
2020-06-12 17:12:41 +1000

    Improved integration test setup
    * Added docker-compose.yml to spin up mock API uServices
    * Fixed github actions to use docker compose stack

---
+++ tests/integration/__init__.py
@@ -0,0 +1,6 @@
+# See docker-compose.yml
+WES_MOCK_EP = "http://0.0.0.0:4010"
+TES_MOCK_EP = "http://0.0.0.0:4011"
+GDS_MOCK_EP = "http://0.0.0.0:4012"
+ENS_MOCK_EP = "http://0.0.0.0:4013"
+CSL_MOCK_EP = "http://0.0.0.0:4014"
