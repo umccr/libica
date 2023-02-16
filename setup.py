3f77a8b77d3a243cfe4e86bfd82abb6456b3300d
Victor San Kho Lin <victor@sankholin.com>
2023-02-16 17:49:56 +1100

    Dropped Python 3.6 support
    * Minimum required is set to Python 3.7
    * Added Python 3.10 and 3.11 to test set

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     extras_require={
         "dev": [
             "pipdeptree",
