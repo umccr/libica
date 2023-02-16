13ae7e9c881aa3c332681a539b042ea9ad4b126b
Victor San Kho Lin <victor@sankholin.com>
2023-02-16 18:15:56 +1100

    Updated ICAv1 Swagger definition 20230216
    * Also bumped version next 2.2.0rc1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="libica",
-    version="2.1.0",
+    version="2.2.0rc1",
     url="https://github.com/umccr-illumina/libica",
     license="MIT",
     author="UMCCR and Contributors",
