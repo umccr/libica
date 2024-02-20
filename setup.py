8ac6e2be957ce28412a065ff2d98e9983d5d258b
Victor San Kho Lin <victor@sankholin.com>
2024-02-20 11:57:19 +1100

    Bumped start of next dev 2.4.0rc1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="libica",
-    version="2.3.0",
+    version="2.4.0rc1",
     url="https://github.com/umccr-illumina/libica",
     license="MIT",
     author="UMCCR and Contributors",
